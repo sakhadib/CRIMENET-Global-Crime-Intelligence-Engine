596d8e6033b77697155fa0627190a9af452565dc
tahsin11rayshad <tahsinislam@iut-dhaka.edu>
2025-08-13 00:48:03 +0600

    google news scraper implemented

--- StrategyScraper/__init__.py
+++ StrategyScraper/__init__.py
@@ -4,5 +4,6 @@
 from .scraper import NewsScraper
 from .bbcNewsScraper import BBCNewsScraper
 from .yahooNewsScraper import YahooNewsScraper
+from .googleNewsScraper import GoogleNewsScraper
 
-__all__ = ['NewsScraper', 'BBCNewsScraper', 'YahooNewsScraper']
+__all__ = ['NewsScraper', 'BBCNewsScraper', 'YahooNewsScraper', 'GoogleNewsScraper']
--- factory/websiteFactory.py
+++ factory/websiteFactory.py
@@ -1,9 +1,11 @@
 from StrategyScraper.bbcNewsScraper import BBCNewsScraper
 from StrategyScraper.aljajiraScraper import AlJazeeraScraper
 from StrategyScraper.yahooNewsScraper import YahooNewsScraper
+from StrategyScraper.googleNewsScraper import GoogleNewsScraper
 
 websites = [
     # {"name": "bbc", "scraper": BBCNewsScraper()},
     # {"name": "aljazeera", "scraper": AlJazeeraScraper()},
-    {"name": "yahoonews", "scraper": YahooNewsScraper()}
+    # {"name": "yahoonews", "scraper": YahooNewsScraper()},
+    {"name": "googlenews", "scraper": GoogleNewsScraper()}
 ]   
